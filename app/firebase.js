4326c711c4ab15742a7feab138a77bd266c3f718
Aanshuu <e21cseu0085@bennett.edu.in>
2024-07-02 08:20:21 +0530

    please

--- app/firebase.js
+++ app/firebase.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+'use client';
 import { initializeApp } from "firebase/app";
 import { getAnalytics, isSupported} from "firebase/analytics";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
