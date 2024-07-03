f16c7660b556e5c5f2d5c53138be7896f8f482b0
Aanshuu <e21cseu0085@bennett.edu.in>
2024-07-03 08:24:35 +0530

    SignIn page designed

--- app/SignIn.js
+++ app/SignIn.js
@@ -16,10 +16,14 @@ const SignIn = ({ setUser }) => {
   return (
     <div className="flex justify-center items-center h-screen">
       <button 
-        className="border-2 border-black p-2 rounded-lg"
-        onClick={signInWithGoogle}>Sign In</button>
+        className="text-4xl font-bold border-2 font-roboto border-black p-2 rounded-lg text-[#6CBF8F] hover:bg-[#000000] transition duration-300 ease-in-out shadow-sm shadow-black"
+        onClick={signInWithGoogle}>
+        Sign In
+      </button>
     </div>
+
   );
 };
 
 export default SignIn;
+// bg-gradient-to-r from-[#240750] via-[#000000] to-[#2c3e50]
--- app/components/landingPage.jsx
+++ app/components/landingPage.jsx
@@ -5,7 +5,7 @@ export default function IntroPage(props){
     return(
         <div className="flex flex-col justify-center items-center h-screen gap-5">
             <h1 className="text-4xl text-[#293264] font-medium">Quizzical</h1>
-            <h3 className="text-[#293264] font-medium">Start the game to know your general knowledge</h3>
+            <h3 className="text-[#293264] font-medium text-sm sm:text-lg">Start the game to know your general knowledge</h3>
             <button className="bg-[#4D5B9E] px-8 py-3 border-2 rounded-lg text-white text-2xl" onClick={props.onStartQuiz}>Start Quiz</button>
         </div>
     )
